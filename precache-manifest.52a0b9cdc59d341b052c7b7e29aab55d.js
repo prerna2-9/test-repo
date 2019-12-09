5bf51c9991b0ac45f885b69c466eb17e12944846
Prerna <bajajprerna0@gmail.com>
2019-12-09 10:53:41 +0530

    Updates

---
+++ precache-manifest.52a0b9cdc59d341b052c7b7e29aab55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ab387a44a5813724d9a37b3723721f",
+    "url": "/test-repo/index.html"
+  },
+  {
+    "revision": "4942882077c3fc84a828",
+    "url": "/test-repo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "48d05651c5bb911b1213",
+    "url": "/test-repo/static/js/2.d5da00ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-repo/static/js/2.d5da00ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4942882077c3fc84a828",
+    "url": "/test-repo/static/js/main.8ae6e99d.chunk.js"
+  },
+  {
+    "revision": "7a8015a254ba419a0c19",
+    "url": "/test-repo/static/js/runtime-main.8eeb0822.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-repo/static/media/logo.5d5d9eef.svg"
+  }
+]);
